425922f8a68a7ec4f2cdcea7227d10799085a1a8
Valentin Berger <V4L3@users.noreply.github.com>
2017-12-04 13:28:49 +0100

    p5 min added

--- _02-Agents/00_02/sketch.js
+++ _02-Agents/00_02/sketch.js
@@ -11,20 +11,38 @@ function setup() {
   var density = displayDensity();
   pixelDensity(density);
   initAgents();
+  background(255);
 }
 
 function draw() {
 
-  //background(255)
+  //drawOverlay();
+
+  background(255, 0)
   for (var i = 0; i < agents.length; i++) {
     agents[i].draw();
   }
 
 }
 
+function drawOverlay() {
+  for (let x = 0; x < windowWidth; x++) {
+    for (let y = 0; y < windowHeight; y++) {
+      if (x >= mouseX + 100 && x + cos(this.angle) <= mouseX - 100) {
+        if (y >= mouseY + 100 && y + sin(this.angle) <= mouseY - 100) {
+          stroke(0);
+          strokeWeight(4)
+          point(x, y);
+        }
+      }
+    }
+  }
+}
+
+
 function initAgents() {
   let angle = random(0, 2 * PI)
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < 100; i++) {
     let a = new Agent(random(0, windowWidth), random(0, windowHeight), 0, 10, random(0, 2 * PI), i)
     agents.push(a)
   }
@@ -39,10 +57,16 @@ class Agent {
     this.offset = 2;
     this.left = true;
     this.angle = angle;
+    this.bounceCount = 5000;
+    this.position = position;
   }
 
   draw() {
-    point(this.x + cos(this.angle), this.y + sin(this.angle) + 2* noise(this.offset), this.s, this.s, );
+    // if(this.x + cos(this.angle) <= mouseX + 100 && this.x + cos(this.angle) >= mouseX - 100){
+    //   if(this.y + sin(this.angle) <= mouseY + 100 && this.x + sin(this.angle) >= mouseY - 100){
+    point(this.x + cos(this.angle), this.y + sin(this.angle) + noise(this.bounceCount), this.s, this.s, );
+    //   }
+    // }
     //this.offset++;
 
     this.movement();
@@ -51,67 +75,79 @@ class Agent {
   }
 
   collisionDetection() {
-    // let pixelColors = get(toInt(this.x), toInt(this.y));
-    // if (color(pixelColors) == color(255)) {
-    //    noLoop();
-    // } else {console.log(brightness((color(pixelColors))))}
 
+    if (this.bounceCount < 0) {
+      agents.splice(this.position, 1);
+    }
 
     if (positions.length > 5) {
       for (let i = 0; i < positions.length - 1; i++) {
         if (this.x <= positions[i].x + 1 && this.x >= positions[i].x - 1) {
           if (this.y <= positions[i].y + 1 && this.y >= positions[i].y - 1) {
-            //console.log("collision");
-            this.angle = random(0,2*PI)
-          }
+
+            
+
+            // if (this.angle > 1.5 * PI) {
+            //   this.angle -= random(0, HALF_PI);
+            // } else if (this.angle > PI) {
+            //   this.angle -= random(0, HALF_PI);
+            // } else if (this.angle > HALF_PI) {
+            //   this.angle -= random(1.5 * PI, 2 * PI)
+            // }
+            // else {
+            //   this.angle -= random(PI, 1.5 * PI)
+            // }
+          this.angle = random(0, 2 * PI);
+          this.bounceCount--;
         }
-
       }
+
     }
-
-
-
-
-
   }
 
 
 
-  reachBorder() {
-    if (this.x > 0) {
-      if (this.x < windowWidth) {
-      }
-      else {
-        // going out on the right
-        // pi random to the left
-        this.angle = random(PI / 2, 3 * PI / 2);
-      }
+
+
+}
+
+
+
+reachBorder() {
+  if (this.x > 0) {
+    if (this.x < windowWidth) {
     }
     else {
-      // going out on the left
-      // pi random to the right
-      this.angle = (-PI / 2) + random(0, PI);
+      // going out on the right
+      // pi random to the left
+      this.angle = random(PI / 2, 3 * PI / 2);
     }
+  }
+  else {
+    // going out on the left
+    // pi random to the right
+    this.angle = (-PI / 2) + random(0, PI);
+  }
 
-    if (this.y > 0) {
-      if (this.y < windowHeight) {
-      }
-      else {
-        // going out on the bottom
-        this.angle = random(PI, TWO_PI);
-      }
+  if (this.y > 0) {
+    if (this.y < windowHeight) {
     }
     else {
-      // going out on the top
-      this.angle = random(0, PI);
+      // going out on the bottom
+      this.angle = random(PI, TWO_PI);
     }
   }
-
-  movement() {
-    positions.push({ x: Math.round(this.x), y: Math.round(this.y) })
-    this.x += cos(this.angle);
-    this.y += sin(this.angle);
+  else {
+    // going out on the top
+    this.angle = random(0, PI);
   }
+}
+
+movement() {
+  positions.push({ x: Math.round(this.x), y: Math.round(this.y) })
+  this.x += cos(this.angle);
+  this.y += sin(this.angle);
+}
 
 }
 
