56e0cee0a3ed801a4724abc228e2e8c8dcb748f0
Valentin Berger <V4L3@users.noreply.github.com>
2017-12-04 10:55:58 +0100

    spider-agents

--- _02-Agents/00_02/sketch.js
+++ _02-Agents/00_02/sketch.js
@@ -1,6 +1,7 @@
 // Global var
 
 var agents = [];
+var positions = [];
 
 function setup() {
   // Canvas setup
@@ -22,15 +23,15 @@ function draw() {
 }
 
 function initAgents() {
-  let angle = random(0, 2*PI)
-  for (var i = 0; i < 100; i++) {
-    let a = new Agent(random(0, windowWidth), random(0, windowHeight), 0, 10, random(0, 2*PI))
+  let angle = random(0, 2 * PI)
+  for (var i = 0; i < 50; i++) {
+    let a = new Agent(random(0, windowWidth), random(0, windowHeight), 0, 10, random(0, 2 * PI), i)
     agents.push(a)
   }
 }
 
 class Agent {
-  constructor(px, py, col, size, angle) {
+  constructor(px, py, col, size, angle, position) {
     this.x = px;
     this.y = py;
     this.c = col;
@@ -41,26 +42,75 @@ class Agent {
   }
 
   draw() {
-    point(this.x + cos(this.angle), this.y + sin(this.angle) + 10 * noise(this.offset), this.s, this.s, );
-    this.offset++;
+    point(this.x + cos(this.angle), this.y + sin(this.angle) + 2* noise(this.offset), this.s, this.s, );
+    //this.offset++;
+
     this.movement();
-    //collisionDetection();
+    this.reachBorder();
+    this.collisionDetection();
   }
 
-  collisionDetection(){
-    let pixelColors = get(toInt(posX),toInt(posY));
-    if ( brightness(color(pixelColors)) != brightness(color(backgroundGrey)) || reachedBorder) {
-      
+  collisionDetection() {
+    // let pixelColors = get(toInt(this.x), toInt(this.y));
+    // if (color(pixelColors) == color(255)) {
+    //    noLoop();
+    // } else {console.log(brightness((color(pixelColors))))}
+
+
+    if (positions.length > 5) {
+      for (let i = 0; i < positions.length - 1; i++) {
+        if (this.x <= positions[i].x + 1 && this.x >= positions[i].x - 1) {
+          if (this.y <= positions[i].y + 1 && this.y >= positions[i].y - 1) {
+            //console.log("collision");
+            this.angle = random(0,2*PI)
+          }
+        }
+
+      }
+    }
+
+
+
+
+
+  }
+
+
+
+  reachBorder() {
+    if (this.x > 0) {
+      if (this.x < windowWidth) {
+      }
+      else {
+        // going out on the right
+        // pi random to the left
+        this.angle = random(PI / 2, 3 * PI / 2);
+      }
+    }
+    else {
+      // going out on the left
+      // pi random to the right
+      this.angle = (-PI / 2) + random(0, PI);
+    }
+
+    if (this.y > 0) {
+      if (this.y < windowHeight) {
+      }
+      else {
+        // going out on the bottom
+        this.angle = random(PI, TWO_PI);
+      }
+    }
+    else {
+      // going out on the top
+      this.angle = random(0, PI);
     }
   }
 
   movement() {
-    if (this.x > windowWidth || this.y > windowHeight) {
-
-    } else {
-      this.x += cos(this.angle);
-      this.y += sin(this.angle);
-    }
+    positions.push({ x: Math.round(this.x), y: Math.round(this.y) })
+    this.x += cos(this.angle);
+    this.y += sin(this.angle);
   }
 
 }
