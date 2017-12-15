1a6a847f2109b15c40a05aa4a5c53cb800fbc6f7
Valentin Berger <V4L3@users.noreply.github.com>
2017-12-15 13:42:50 +0100

    title sketch changes

--- sketch.js
+++ sketch.js
@@ -1,110 +1,130 @@
-// Based on the code M_1_5_01.pde from
-// Generative Gestaltung, ISBN: 978-3-87439-759-9
-
 // Global var
-// Some of the var might be initialised in gui.js
-var agents, density, letterPositions;
+
+var agents = [];
+//var positions = [];
+var gridResolutionX;
+var gridResolutionY;
+var tileSize = 50;
+var increment = 0;
 
 function setup() {
   // Canvas setup
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5Container");
   // Detect screen density (retina)
-  // Comment it out if the sketch is too slow
-  density = displayDensity();
+  var density = displayDensity();
   pixelDensity(density);
-  // Init var
-  // some of the var might be initialised in gui.js
-  backgroundGrey = 0;
-  background(backgroundGrey);
-  // Init 
-  initScene();
-  // Draw text
-  noStroke();
-  fill(backgroundGrey, options.txtAlpha);
-  textSize(options.txtSize);
-  text(options.txt, width / 2 - textWidth(options.txt) / 2, height / 2 + options.txtSize / 2);
+  background(255);
+  gridResolutionX = round(width / tileSize) + 1;
+  gridResolutionY = round(height / tileSize) + 1;
+  initAgentsOnGrid();
+  smooth();
+  rectMode(CENTER);
+  noLoop();
+
+
+  // for (var gridY = 0; gridY < gridResolutionY; gridY++) {
+  //   for (var gridX = 0; gridX < gridResolutionX; gridX++) {
+  //     let posX = tileSize * gridX - tileSize / 2;
+  //     let posY = tileSize * gridY - tileSize / 2;
+  //     strokeWeight(0.15);
+  //     fill(255);
+  //     // save drawing state for later
+  //     push();
+
+  //     // move the origin to the pivot point
+  //     translate(posX, posY);
+
+  //     // then pivot the grid
+  //     rotate(radians(90));
+
+  //     rect(0, 0, tileSize, tileSize);
+
+  //     //revert to original drawing state
+  //     pop();
+
+  //     strokeWeight(3);
+  //     point(posX, posY)
+
+
+  //   }
+  // }
+
+  opentype.load('fonts/linowrite.ttf', function (err, font) {
+    if (err) {
+      alert('Font could not be loaded: ' + err);
+    } else {
+      var ctx = document.getElementById('defaultCanvas0').getContext('2d');
+      // Construct a Path object containing the letter shapes of the given text.
+      // The other parameters are x, y and fontSize.
+      // Note that y is the position of the baseline.
+      var myGlyphs = font.stringToGlyphs('Digital Ideation');
+      // If you just want to draw the text you can also use font.draw(ctx, text, x, y, fontSize).
+
+
+      myGlyphs.forEach(element => {
+        //console.log(element)
+        push();
+        translate(100 + increment, 100)
+        scale(1, -1);
+        fill(random(0))
+        beginShape();
+        element.path.commands.forEach(point => {
+          vertex(point.x / 10, point.y / 10)
+        })
+        endShape();
+        pop();
+
+        //console.log(element.getBoundingBox().x2 - element.getBoundingBox().x1)
+        //let offset = element.getBoundingBox().x2
+        if (element.name != "space") {
+          increment += element.xMax / 10;
+        } else {
+          increment += 60
+        }
+      })
+    }
+  });
+
 }
 
 function draw() {
 
-  // 
-  smooth();
-  background(backgroundGrey, options.overlayAlpha);
-  stroke(255, options.agentsAlpha);
+  let faderX = mouseX / width;
+  let t = millis() / 1000;
 
-  noiseDetail(options.octaves, options.falloff);
 
-  // Draw agents  
-  for (var i = 0; i < agents.length - 1; i++) {
-    agents[i].draw(options.noiseScale, options.noiseStrength, options.strokeWidth, options.drawMode);
-  }
-
-  
 
 }
 
+function initAgentsOnGrid() {
+  let angle = random(0, 2 * PI)
+  for (var gridY = 0; gridY < gridResolutionY; gridY++) {
+    for (var gridX = 0; gridX < gridResolutionX; gridX++) {
+      let posX = tileSize * gridX + tileSize / 2;
+      let posY = tileSize * gridY + tileSize / 2;
 
-function initScene() {
-  background(0);
-  fill(1);
-  textSize(options.txtSize);
-  text(options.txt, width / 2 - textWidth(options.txt) / 2, height / 2 + options.txtSize / 2);
-
-  // Load pixels
-  letterPositions = [];
-  agents = [];
-  var step = options.step;
-  let w = width * density;
-  let h = height * density;
-  let container = document.getElementById('p5Container');
-  ctx = container.firstChild.getContext("2d");
-  let data = ctx.getImageData(0, 0, w, h).data;
-  let position = 0;
-  for (var i = 0; i < w; i += step) {
-    for (var j = 0; j < h; j += step) {
-      if (data[((i + j * w) * 4) + 1] == 1) {
-        letterPositions.push({ x: Math.round(this.x), y: Math.round(this.y) })
-        let a0 = new Agent(i/density, j/density, random(0, 2 * PI), position)
-        agents.push(a0);
-        position++;
-      }
+      let a0 = new Agent(posX, posY, 0, tileSize, random(0, 2 * PI), gridX + gridY)
+      agents.push(a0)
     }
   }
+
 }
 
-function keyReleased() {
-  if (keyCode == DELETE || keyCode == BACKSPACE) background(backgroundGrey);
-  if (keyCode == 32) {
-    for (var i = 0; i < agents.length - 1; i++) agents[i].restart();
-  }
+
+
+function keyPressed() {
   if (key == 's' || key == 'S') saveThumb(650, 350);
-
-  if (key == '1') options.drawMode = 1;
-  if (key == '2') options.drawMode = 2;
-  if (key == ' ') {
-    let newNoiseSeed = floor(random(100000));
-    noiseSeed(newNoiseSeed);
-  }
-
-  if (keyCode == UP_ARROW) options.falloff += 0.05;
-  if (keyCode == DOWN_ARROW) options.falloff -= 0.05;
-  if (options.falloff > 1.0) options.falloff = 1.0;
-  if (options.falloff < 0.0) options.falloff = 0.0;
-
-  if (keyCode == LEFT_ARROW) options.octaves--;
-  if (keyCode == RIGHT_ARROW) options.octaves++;
-  if (options.octaves < 0) options.octaves = 0;
-
 }
 
 // Tools
+
 // resize canvas when the window is resized
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight, false);
 }
 
-//  conversion
+// Int conversion
 function toInt(value) {
   return ~~value;
 }
