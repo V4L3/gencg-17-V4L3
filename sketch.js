1c768c31297f769f16e5b1948cada1193021280f
Valentin Berger <V4L3@users.noreply.github.com>
2017-12-15 13:40:52 +0100

    changed title sketch

--- sketch.js
+++ sketch.js
@@ -1,73 +1,110 @@
-// Based on the code P_2_1_1_01.pde from
+// Based on the code M_1_5_01.pde from
 // Generative Gestaltung, ISBN: 978-3-87439-759-9
 
 // Global var
-var tileCount, actRandomSeed, actStrokeCap;
- 
+// Some of the var might be initialised in gui.js
+var agents, density, letterPositions;
+
 function setup() {
   // Canvas setup
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5Container");
   // Detect screen density (retina)
-  var density = displayDensity();
+  // Comment it out if the sketch is too slow
+  density = displayDensity();
   pixelDensity(density);
-  // Colors and drawing modes
-  smooth();
-  // Init Var
-  tileCount = 20;
-  actRandomSeed = 0;
-  actStrokeCap = ROUND; 
+  // Init var
+  // some of the var might be initialised in gui.js
+  backgroundGrey = 0;
+  background(backgroundGrey);
+  // Init 
+  initScene();
+  // Draw text
+  noStroke();
+  fill(backgroundGrey, options.txtAlpha);
+  textSize(options.txtSize);
+  text(options.txt, width / 2 - textWidth(options.txt) / 2, height / 2 + options.txtSize / 2);
 }
 
 function draw() {
-  // Canvas draw options
-  background(255);
+
+  // 
   smooth();
-  noFill();
+  background(backgroundGrey, options.overlayAlpha);
+  stroke(255, options.agentsAlpha);
 
-  // Stroke options
-  strokeCap(actStrokeCap);
-  randomSeed(actRandomSeed);
+  noiseDetail(options.octaves, options.falloff);
 
-  for (let gridX = 0; gridX < tileCount; gridX++) {
-    for (let gridY = 0; gridY < tileCount; gridY++) {
+  // Draw agents  
+  for (var i = 0; i < agents.length - 1; i++) {
+    agents[i].draw(options.noiseScale, options.noiseStrength, options.strokeWidth, options.drawMode);
+  }
 
-      let posX = width / tileCount * gridX;
-      let posY = width / tileCount * gridY;
+  
 
-      let toggle = toInt(random(0, 2));
+}
 
-      if (toggle == 0) {
-        strokeWeight(mouseX / 20);
-        line(posX, posY, posX + width / tileCount, posY + height / tileCount);
 
-      } else if (toggle == 1) {
-        strokeWeight(mouseY / 20);
-        line(posX, posY + width / tileCount, posX + height / tileCount, posY);
+function initScene() {
+  background(0);
+  fill(1);
+  textSize(options.txtSize);
+  text(options.txt, width / 2 - textWidth(options.txt) / 2, height / 2 + options.txtSize / 2);
+
+  // Load pixels
+  letterPositions = [];
+  agents = [];
+  var step = options.step;
+  let w = width * density;
+  let h = height * density;
+  let container = document.getElementById('p5Container');
+  ctx = container.firstChild.getContext("2d");
+  let data = ctx.getImageData(0, 0, w, h).data;
+  let position = 0;
+  for (var i = 0; i < w; i += step) {
+    for (var j = 0; j < h; j += step) {
+      if (data[((i + j * w) * 4) + 1] == 1) {
+        letterPositions.push({ x: Math.round(this.x), y: Math.round(this.y) })
+        let a0 = new Agent(i/density, j/density, random(0, 2 * PI), position)
+        agents.push(a0);
+        position++;
       }
     }
   }
 }
 
-function mousePressed() {
-  actRandomSeed = toInt(random(100000));
-}
-
-function keyPressed() {
+function keyReleased() {
+  if (keyCode == DELETE || keyCode == BACKSPACE) background(backgroundGrey);
+  if (keyCode == 32) {
+    for (var i = 0; i < agents.length - 1; i++) agents[i].restart();
+  }
   if (key == 's' || key == 'S') saveThumb(650, 350);
-  if (key == '1') actStrokeCap = ROUND;
-  if (key == '2') actStrokeCap = SQUARE;
-  if (key == '3') actStrokeCap = PROJECT;
+
+  if (key == '1') options.drawMode = 1;
+  if (key == '2') options.drawMode = 2;
+  if (key == ' ') {
+    let newNoiseSeed = floor(random(100000));
+    noiseSeed(newNoiseSeed);
+  }
+
+  if (keyCode == UP_ARROW) options.falloff += 0.05;
+  if (keyCode == DOWN_ARROW) options.falloff -= 0.05;
+  if (options.falloff > 1.0) options.falloff = 1.0;
+  if (options.falloff < 0.0) options.falloff = 0.0;
+
+  if (keyCode == LEFT_ARROW) options.octaves--;
+  if (keyCode == RIGHT_ARROW) options.octaves++;
+  if (options.octaves < 0) options.octaves = 0;
+
 }
 
 // Tools
-
 // resize canvas when the window is resized
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight, false);
 }
 
-// Int conversion
+//  conversion
 function toInt(value) {
   return ~~value;
 }
@@ -79,6 +116,6 @@ function timestamp() {
 
 // Thumb
 function saveThumb(w, h) {
-  let img = get( width/2-w/2, height/2-h/2, w, h);
-  save(img,'thumb.jpg');
+  let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
+  save(img, 'thumb.jpg');
 }
